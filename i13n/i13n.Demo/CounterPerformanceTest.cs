f668c59efbcb825518988d7bb0aedc8f0a1f7668
Steve Cote <sdcote@aep.com>
2020-06-05 15:27:56 -0400

    minor improvements to formatting

--- i13n/i13n.Demo/CounterPerformanceTest.cs
+++ i13n/i13n.Demo/CounterPerformanceTest.cs
@@ -26,16 +26,14 @@ namespace i13n.Demo
             }
 
 
-
             // 1,645,460 calls per second - Debug mode,  1,948,128.6 - Command Line (Intel i5-6300U # 2.4GHz - 2 cores, 4 processors) Windows 10
-
             Console.WriteLine("Throughput = " + ((float)(totalCount / (float)totalElapsed * 100000000) / runs) + " calls per second");
         }
 
         private static long runTest(Monitor scorecard)
         {
-            long started =DateTime.Now.Ticks;
-            long end = started + 10000*10000;
+            long started = DateTime.Now.Ticks;
+            long end = started + 10000 * 10000;
             while (DateTime.Now.Ticks <= end)
             {
                 scorecard.Increment("DemoCounter");
--- i13n/i13n.Demo/TimerPerformanceTest.cs
+++ i13n/i13n.Demo/TimerPerformanceTest.cs
@@ -27,15 +27,14 @@ namespace i13n.Demo
             totalCount = timer.StartCount;
 
 
-            // 1,645,460 calls per second - Debug mode,  1,948,128.6 - Command Line (Intel i5-6300U # 2.4GHz - 2 cores, 4 processors) Windows 10
-
+            // 436,937.8 calls per second - Debug mode,  482,567.25 - Command Line (Intel i5-6300U # 2.4GHz - 2 cores, 4 processors) Windows 10
             Console.WriteLine("Throughput = " + ((float)(totalCount / (float)totalElapsed * 100000000) / runs) + " calls per second");
         }
 
         private static long runTest(ITimer timer)
         {
-            long started =DateTime.Now.Ticks;
-            long end = started + 10000*10000;
+            long started = DateTime.Now.Ticks;
+            long end = started + 10000 * 10000;
             while (DateTime.Now.Ticks <= end)
             {
                 timer.Start();
